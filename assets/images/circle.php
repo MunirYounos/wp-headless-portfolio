d83b614ecdec9b604b8ebde226cb0943f00c7b3b
Munir <munir@anodyne.dk>
2019-12-23 15:38:04 +0100

    New cv section added

---
+++ assets/images/circle.php
@@ -0,0 +1,5 @@
+<svg class="circle" fill="none">
+<rect width="100%" height="100%" fill="white"/>
+<circle class="circle gray" cx="50" cy="50" r="39" stroke="#E9E9E9" stroke-width="8"/>
+<path class="circle color" fill-rule="evenodd" clip-rule="evenodd" d="M50 7C26.2518 7 7 26.2518 7 50C7 73.7482 26.2518 93 50 93V85C30.67 85 15 69.33 15 50C15 30.67 30.67 15 50 15V7Z" fill="#E0A8A8"/>
+</svg>
--- assets/images/mobile.php
+++ assets/images/mobile.php
@@ -1,4 +1,4 @@
-<svg class="mobile-icon">
+<svg class="mobile-icon ico">
 <rect class="mobile body" x="1.5" y="1.5" rx="3.5"/>
 <rect class="mobile round" x="23.5" y="70"  rx="7.5"/>
 <rect class="mobile top" x="23.5" y="8" width="11" height="2" rx="1"/>
